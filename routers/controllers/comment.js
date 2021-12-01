286ce3694b2d3b825b95213598a7f4bce832203f
Sulaiman122 <33493055+Sulaiman122@users.noreply.github.com>
2021-12-01 22:43:03 +0300

    v5

--- routers/controllers/comment.js
+++ routers/controllers/comment.js
@@ -1,5 +1,4 @@
-const postModel = require("../../db/models/post");
-const likeModel = require("../../db/models/like");
+const commentModel = require("../../db/models/comment");
 const roleModel = require("../../db/models/role");
 
 
@@ -9,7 +8,7 @@ const createComment = (req, res) => {
   const { comment , isDeleted } = req.body;
 
 
-  const newComment = new postModel({ comment, isDeleted, user: req.token.id, post:id });
+  const newComment = new commentModel({ comment, isDeleted, user: req.token.id, post:id });
   newComment
     .save()
     .then((result) => {
@@ -25,7 +24,7 @@ const deleteComment = async (req, res) => {
   const { id } = req.params;
   let sameUser = false;
 
-  await postModel.findOne({ _id: id, user: req.token.id }).then((result) => {
+  await commentModel.findOne({ _id: id, user: req.token.id }).then((result) => {
     if (result) {
       sameUser = true;
     }
@@ -33,54 +32,52 @@ const deleteComment = async (req, res) => {
 
   const result = await roleModel.findById(req.token.role);
 
-  //here we check if it's Admin user OR the same user who created the post
+  //here we check if it's Admin user OR the same user who created the comment
   if (result.role == "admin" || sameUser) {
-    postModel
+    commentModel
       .findByIdAndUpdate(id, { $set: { isDeleted: true } })
       .then((result) => {
         if (result) {
-          res.status(200).json("post removed");
+          res.status(200).json("comment removed");
         } else {
-          res.status(404).json("post does not exist");
+          res.status(404).json("comment does not exist");
         }
       })
       .catch((err) => {
         res.status(400).json(err);
       });
   } else {
-    res.json("you don't have the priveleges to remove the post");
+    res.json("you don't have the priveleges to remove the comment");
   }
 };
 
 const updateComment = async (req, res) => {
   const { id } = req.params;
-  const { desc, img } = req.body;
+  const { comment } = req.body;
   let sameUser = false;
 
-  await postModel.findOne({ _id: id, user: req.token.id }).then((result) => {
+  await commentModel.findOne({ _id: id, user: req.token.id }).then((result) => {
     if (result) {
       sameUser = true;
     }
   });
 
-  const result = await roleModel.findById(req.token.role);
-
-  //here we check if it's Admin user OR the same user who created the post
-  if (result.role == "admin" || sameUser) {
-    postModel
-      .findByIdAndUpdate(id, { $set: { desc: desc, img: img } })
+  //here we check if it's the same user who created the comment
+  if (sameUser) {
+    commentModel
+      .findByIdAndUpdate(id, { $set: { comment: comment } })
       .then((result) => {
         if (result) {
-          res.status(200).json("post updated");
+          res.status(200).json("comment updated");
         } else {
-          res.status(404).json("post does not exist");
+          res.status(404).json("comment does not exist");
         }
       })
       .catch((err) => {
         res.status(400).json(err);
       });
   } else {
-    res.json("you don't have the priveleges to update the post");
+    res.json("you don't have the priveleges to update the comment");
   }
 };
 
